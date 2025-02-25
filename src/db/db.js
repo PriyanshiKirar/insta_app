72e5592cdd77cead1050d775ccf6cac4492a4897
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2025-02-25 21:49:12 +0530

    25fev

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,7 @@ import config  from "../config/config.js";
 export const connect=()=>{
     mongoose.connect(config.MONGO_URI)
     .then(()=>{
-      console.log("connect to db");
+      console.log("connected to db");
         
     }).catch((err=>{
         console.log("Error",err.message);
