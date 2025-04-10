354e0a08d4e62c8ce58128db2a72350cef1d6fe2
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2025-04-10 23:10:25 +0530

    10 april

--- src/db/db.js
+++ src/db/db.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
-import config  from "../config/config.js";
+import config from "../config/config.js";
 
-export const connect=()=>{
-    mongoose.connect(config.MONGO_URI)
-    .then(()=>{
+export const connect = () => {
+  mongoose
+    .connect(config.MONGO_URI)
+    .then(() => {
       console.log("connected to db ");
-        
-    }).catch((err=>{
-        console.log("error",err.message);
-        
-    }))
-}
+    })
+    .catch((err) => {
+      console.log("error", err.message);
+    });
+};
 
 export default connect;
--- src/services/redisService.js
+++ src/services/redisService.js
@@ -1,13 +1,12 @@
-import Redis from 'ioredis'
-import config from '../config/config.js'
-const redis=new Redis({
-    host:config.REDIS_HOST,
-    port:config.REDIS_PORT,
-    password:config.REDIS_PASSWORD,
-})
+import Redis from "ioredis";
+import config from "../config/config.js";
+const redis = new Redis({
+  host: config.REDIS_HOST,
+  port: config.REDIS_PORT,
+  password: config.REDIS_PASSWORD,
+});
 
-redis.on("connect",()=>{
-    console.log("redis connceted to ");
-    
-})
-export default redis; 
+redis.on("connect", () => {
+  console.log("redis connceted to ");
+});
+export default redis;
